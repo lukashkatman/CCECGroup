1a06ae3c1dc1eba168b74c16fec98e086bc6d5bb
lisaenglund <lisaenglund89@gmail.com>
2015-04-20 15:46:13 +0100

    Project 2 CCEC

---
+++ src/ccec/Scene1LogInController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ccec;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Lisa
+ */
+public class Scene1LogInController implements Initializable {
+
+    @FXML
+    private TextField usernameLogIn;
+
+    @FXML
+    private PasswordField passwordLogIn;
+
+    @FXML
+    private void handleButtonLogInAction(ActionEvent event) {
+
+        try {
+            // An alternative to getting the Stage from a Node declared in the scene is
+            // to ask the button pressed what scene it belongs to.
+            Node node = (Node) event.getSource();       
+            Stage stage = (Stage) node.getScene().getWindow(); 
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene2Admin.fxml"));  
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);      
+            stage.setScene(scene);              
+            stage.show();                       
+
+            System.out.println("You clicked LOG IN!");
+        } catch (Exception ex) {
+        }
+
+        System.out.println("ERROR!");
+    }
+
+    @FXML
+    private void handleButtonExitAction(ActionEvent event) {
+        System.out.println("You clicked EXIT!");
+        System.exit(0);
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
---
+++ src/ccec/Scene2AdminController.java
@@ -0,0 +1,78 @@
+package ccec;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author Lisa
+ */
+public class Scene2AdminController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    private void handleButtonStudentsAction(ActionEvent event) {
+
+        try {
+            // An alternative to getting the Stage from a Node declared in the scene is
+            // to ask the button pressed what scene it belongs to.
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Students.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+            System.out.println("You clicked STUDENTS!");
+        } catch (Exception ex) {
+        }
+
+        System.out.println("ERROR!");
+    }
+
+    @FXML
+    private void handleButtonLogOutAction(ActionEvent event) {
+
+        try {
+            // An alternative to getting the Stage from a Node declared in the scene is
+            // to ask the button pressed what scene it belongs to.
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Scene1LogIn.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+            System.out.println("You clicked LOG OUT!");
+        } catch (Exception ex) {
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+}
